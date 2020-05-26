151e1a9418da3a4b43d8d24715455a6aca174700
mart-123 <torchbeam@gmail.com>
2020-05-26 19:17:09 +0100

    end of section 3

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.DTOs;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -11,22 +17,28 @@ namespace DatingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
 
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(UserForRegisterDTO userForRegisterDTO)
+        public async Task<IActionResult> Register([FromBody] UserForRegisterDTO userForRegisterDTO)
         {
-            // validate request
-            userForRegisterDTO.Username = userForRegisterDTO.Username.ToLower();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // validate request (this now happens by virtue of 'ApiController' directive above)
+            // userForRegisterDTO.Username = userForRegisterDTO.Username.ToLower();
 
             if (await _repo.UserExists(userForRegisterDTO.Username))
                 return BadRequest("username already exists");
 
-            /* TERRIBLE PRACTICE?! INSTANTIATING A SKELETON OBJECT TO HOLD A DISEMBODIED PROPERTY!!! */
+            /* TERRIBLE PRACTICE?! OR 'LAZY' (DEFERRED CREATION - OPTIMISATION STRATEGY) INSTANTIATING A */
+            /* SKELETON OBJECT TO HOLD A DISEMBODIED PROPERTY WITH OTHER PROPERTIES STILL BEING NULL!!! */
             /* IT IS A MEANINGLESS OBJECT, HAVING HARDLY ANY OF THE PROPERTIES ESSENTIAL TO A USER!! */
             var userToCreate = new User
             {
@@ -36,6 +48,45 @@ namespace DatingApp.API.Controllers
             var createdUser = await _repo.Register(userToCreate, userForRegisterDTO.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDTO userForLoginDTO)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDTO.Username.ToLower(), userForLoginDTO.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            // generate a token...
+            // starting with some basic identifying details of the user
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.UserName)
+            };
+
+            // generate a hashed security key to sign the token
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+            .GetBytes(_config.GetSection("AppSettings:Token").Value));
+            
+            // generate signing credentials (by hashing the key)
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {             // anonymous type
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
